7810ed95578f3365a24033f7ab23ff51d11a6237
srfoster <stephen@thoughtstem.com>
2019-12-11 09:33:14 -0800

    city deps

--- info.rkt
+++ info.rkt
@@ -2,7 +2,14 @@
 (define collection "metacoders-dot-org-core-pages")
 (define deps '("base"
                "threading"
-               "https://github.com/thoughtstem/metacoders-dot-org.git?path=metacoders-dot-org-lib"))
+               "https://github.com/thoughtstem/metacoders-dot-org.git?path=metacoders-dot-org-lib"
+               "https://github.com/thoughtstem/metacoders-dot-org-dc.git?path=metacoders-dot-org-dc-site"
+               "https://github.com/thoughtstem/metacoders-dot-org-reno.git?path=metacoders-dot-org-reno-site"
+               "https://github.com/thoughtstem/metacoders-dot-org-dallas.git?path=metacoders-dot-org-dallas-site"
+               "https://github.com/thoughtstem/metacoders-dot-org-temecula.git?path=metacoders-dot-org-temecula-site"
+               "https://github.com/thoughtstem/metacoders-dot-org-minneapolis.git?path=metacoders-dot-org-minneapolis-site"
+               "https://github.com/thoughtstem/metacoders-dot-org-poway.git?path=metacoders-dot-org-poway-site"
+               "https://github.com/thoughtstem/metacoders-dot-org-chula-vista.git?path=metacoders-dot-org-chula-vista-site"))
 (define build-deps '("scribble-lib" "racket-doc" "rackunit-lib"))
 (define scribblings '(("scribblings/metacoders-dot-org-core-pages.scrbl" ())))
 (define pkg-desc "Description Here")
