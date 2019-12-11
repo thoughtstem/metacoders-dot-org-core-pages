b7d015a71528acc5af2c66e5a43c25a3d27fd604
srfoster <stephen@thoughtstem.com>
2019-12-11 09:28:35 -0800

    threading dep

--- info.rkt
+++ info.rkt
@@ -1,6 +1,7 @@
 #lang info
 (define collection "metacoders-dot-org-core-pages")
 (define deps '("base"
+               "threading"
                "https://github.com/thoughtstem/metacoders-dot-org.git?path=metacoders-dot-org-lib"))
 (define build-deps '("scribble-lib" "racket-doc" "rackunit-lib"))
 (define scribblings '(("scribblings/metacoders-dot-org-core-pages.scrbl" ())))
